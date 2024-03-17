1c02ee9e346d52a7dc6462b814f297dce7ed0033
Miguel <miguel.junior7@hotmail.com>
2024-03-17 17:31:17 -0300

    feat: Added car service

---
+++ src/services/CarService.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../database/database";
+import { CarCreate, CarReturn, CarUpdate } from "../interfaces";
+import { carReturnSchema } from "../schemas";
+
+export class CarService {
+  public create = async (data: CarCreate): Promise<CarReturn> => {
+    const newCar = await prisma.car.create({ data });
+
+    return carReturnSchema.parse(newCar);
+  };
+
+  public read = async (): Promise<Array<CarReturn>> => {
+    const allCars = await prisma.car.findMany();
+
+    return carReturnSchema.array().parse(allCars);
+  };
+
+  public retrieve = async (foundCar: Car): Promise<CarReturn> => {
+    return carReturnSchema.parse(foundCar);
+  };
+
+  public update = async (
+    carId: number,
+    data: CarUpdate
+  ): Promise<CarReturn> => {
+    const updateCar = await prisma.car.update({ where: { id: carId }, data });
+
+    return carReturnSchema.parse(updateCar);
+  };
+
+  public delete = async (carId: number): Promise<void> => {
+    const deleteCar = await prisma.car.delete({ where: { id: carId } });
+  };
+}
