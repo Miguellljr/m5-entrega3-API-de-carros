54da3a787c3ed7c7cf2b66c714a361eecade3040
Miguel <miguel.junior7@hotmail.com>
2024-03-17 23:05:25 -0300

    feat: Added middlewares

---
+++ src/middlewares/ensure.middleware.ts
@@ -0,0 +1,30 @@
+import { Response, Request, NextFunction } from "express";
+import { prisma } from "../database/database";
+import { AppError } from "../errors";
+import { AnyZodObject } from "zod";
+
+export class EnsureMiddleware {
+  public ValidBody =
+    (schema: AnyZodObject) =>
+    (req: Request, _: Response, next: NextFunction): void => {
+      req.body = schema.parse(req.body);
+      return next();
+    };
+
+  public paramsCarIdExists = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const { id } = req.params;
+
+    const foundCar = await prisma.car.findFirst({ where: { id: Number(id) } });
+
+    if (!foundCar) {
+      throw new AppError("Car not found", 404);
+    }
+
+    res.locals = { ...res.locals, foundCar };
+    return next();
+  };
+}
---
+++ src/middlewares/index.ts
@@ -0,0 +1,4 @@
+import { HandleErrorMiddleware } from "./handleError.middleware";
+import { EnsureMiddleware } from "./ensure.middleware";
+
+export { HandleErrorMiddleware, EnsureMiddleware };
